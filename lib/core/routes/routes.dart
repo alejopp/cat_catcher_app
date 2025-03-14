b4984ff25dbba20f28a3b5e45f544c30a5cf33db
Yesid Pelaez <alejo51120@gmail.com>
2025-03-14 03:27:28 -0500

    feat: Add icon launcher. Create Splash screen.

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,15 +1,21 @@
 import 'package:cat_catcher_app/features/cat/domain/entities/cat.dart';
 import 'package:cat_catcher_app/features/cat/presentation/screens/cat_detail_screen.dart';
 import 'package:cat_catcher_app/features/cat/presentation/screens/landing_screen.dart';
+import 'package:cat_catcher_app/features/splash/presentation/screen/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class Routes {
-  static String landingScreen = "/";
+  static String splashScreen = "/";
+  static String landingScreen = "/landing_screen";
   static String catDetailScreen = "/detail_screen";
 }
 
 final routes = GoRouter(
   routes: [
+    GoRoute(
+      path: Routes.splashScreen,
+      builder: (context, state) => const SplashScreen(),
+    ),
     GoRoute(
       path: Routes.landingScreen,
       builder: (context, state) => const LandingScreen(),
---
+++ lib/features/splash/presentation/screen/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:cat_catcher_app/core/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() => _opacity = 1.0);
+    });
+    Future.delayed(Duration(seconds: 5), () {
+      if (context.mounted) {
+        context.go(Routes.landingScreen);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF330072),
+      body: Center(
+        child: AnimatedOpacity(
+          duration: Duration(seconds: 2),
+          opacity: _opacity,
+          child: Image.asset('assets/images/white_cat.png', width: 150),
+        ),
+      ),
+    );
+  }
+}
